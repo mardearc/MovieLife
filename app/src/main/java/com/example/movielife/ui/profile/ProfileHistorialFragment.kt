c7b81ce0ad034dffd34f24663e01ac42f6c0c83f
trianafotografia <triana.jimeno02gmail.com>
2025-05-13 18:55:55 +0200

    [ADD] fragment_profile_historial

--- app/src/main/java/com/example/movielife/ui/profile/ProfileHistorialFragment.kt
+++ app/src/main/java/com/example/movielife/ui/profile/ProfileHistorialFragment.kt
@@ -1,41 +1,62 @@
 package com.example.movielife.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.movielife.ApiService
+import com.example.movielife.DetailPeliculaActivity
+import com.example.movielife.DetailPeliculaActivity.Companion.EXTRA_ID
+import com.example.movielife.PeliculaAdapter
+import com.example.movielife.PeliculaItemResponse
 import com.example.movielife.PostAdapter
 import com.example.movielife.PostPelicula
 import com.example.movielife.R
 import com.example.movielife.User
 import com.example.movielife.databinding.FragmentHomeBinding
 import com.example.movielife.databinding.FragmentProfileHistorialBinding
+import com.example.movielife.databinding.FragmentWatchlistBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.GenericTypeIndicator
 import com.google.firebase.database.ValueEventListener
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class ProfileHistorialFragment : Fragment(){
 
     private lateinit var uid: String
     private lateinit var binding:FragmentProfileHistorialBinding
-    private val postList = mutableListOf<PostPelicula>()
-    private val userMap = mutableMapOf<String, User>()
-    private lateinit var adapter: PostAdapter
+
+    private lateinit var retrofit: Retrofit
+    private lateinit var adapter: PeliculaAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = FragmentProfileHistorialBinding.inflate(inflater, container, false)
+
+        retrofit = getRetrofit()
+        initUI()
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        adapter = PostAdapter(postList, userMap)
-        binding.recyclerViewHistorial.layoutManager = LinearLayoutManager(requireContext())
+        adapter = PeliculaAdapter { navigateToDetail(it) }
+        binding.recyclerViewHistorial.setHasFixedSize(true)
+        binding.recyclerViewHistorial.layoutManager = GridLayoutManager(requireContext(), 4)
         binding.recyclerViewHistorial.adapter = adapter
     }
 
@@ -51,6 +72,79 @@ class ProfileHistorialFragment : Fragment(){
             fragment.arguments = args
             return fragment
         }
+    }
+
+
+
+    private fun initUI() {
+        adapter = PeliculaAdapter { navigateToDetail(it) }
+        binding.recyclerViewHistorial.setHasFixedSize(true)
+        binding.recyclerViewHistorial.layoutManager = GridLayoutManager(requireContext(), 4)
+        binding.recyclerViewHistorial.adapter = adapter
+        searchPeliculasVistas()
+    }
+
+    private fun searchPeliculasVistas() {
+        val uid = FirebaseAuth.getInstance().currentUser?.uid ?: return
+        val database = FirebaseDatabase.getInstance().reference.child("usuarios").child(uid)
+
+        binding.pbBuscador.isVisible = true
+        database.child("peliculasVistas").get().addOnSuccessListener { snapshot ->
+            val idList = snapshot.getValue(object : GenericTypeIndicator<List<String>>() {}) ?: listOf()
+            if (idList.isNotEmpty()) {
+                getPeliculaData(idList)
+            } else {
+                requireActivity().runOnUiThread {
+                    adapter.updateList(emptyList())
+                    binding.pbBuscador.isVisible = false
+                }
+            }
+        }
+    }
+
+    private fun getPeliculaData(ids: List<String>) {
+        val apiService = retrofit.create(ApiService::class.java)
+        val apiKey = "cef2d5efc3c68480cb48f48b33b29de4"
+        val language = "es-ES"
+
+        val peliculasList = mutableListOf<PeliculaItemResponse>()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            for (id in ids) {
+                try {
+                    val response = apiService.getMovieByIdToList(id.toInt(), apiKey, language)
+                    if (response.isSuccessful) {
+                        response.body()?.let { pelicula ->
+                            peliculasList.add(pelicula)
+                        }
+                    }
+                } catch (e: Exception) {
+                    Log.e("mardearc", "Error cargando ID $id: ${e.message}")
+                }
+            }
+
+            requireActivity().runOnUiThread {
+                adapter.updateList(peliculasList)
+                binding.pbBuscador.isVisible = false
+            }
+        }
+    }
+
+
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit
+            .Builder()
+            .baseUrl("https://api.themoviedb.org/3/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    }
+
+    private fun navigateToDetail(id: Int) {
+        val intent = Intent(requireContext(), DetailPeliculaActivity::class.java)
+        intent.putExtra(EXTRA_ID, id)
+        startActivity(intent)
     }
 
 
